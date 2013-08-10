246ded8a77e761a2ed9154243c004f77bd1d71ac
Stephen Heron <stephen.heron@gmail.com>
2013-08-10 11:15:37 +0100

    Updated the DateHelper so that it can handle when we don't have a next meetup, added a edpug.php config file which currently has the default start date and time

---
+++ app/config/edpug.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+
+	'day'             => 'Tuesday',
+  'time'	          => '19:00',
+  'day_repetition'  => 'third'
+
+);
--- app/lib/edPUG/Helpers/DateHelper.php
+++ app/lib/edPUG/Helpers/DateHelper.php
@@ -4,10 +4,13 @@ namespace edPUG\Helpers;
 
 class DateHelper {
 
-  public static function timeUntilNextMeetup(){
-    $nextMeetup = \Meetup::getNextMeetup()->getStartDateTime();
+  public static function timeUntilNextMeetup()
+  {
+
+    $nextMeetupDateTime = self::calculateDateTimeUtilNextMeetup();
+
     $now = new \DateTime();
-    $difference = $nextMeetup->diff($now);
+    $difference = $nextMeetupDateTime->diff($now);
 
     $timeRemaining = $difference->format('%a days');
 
@@ -17,6 +20,34 @@ class DateHelper {
 
     return $timeRemaining;
     
+  }
+
+  public static function calculateDateTimeUtilNextMeetup()
+  {
+    $nextMeetup = \Meetup::getNextMeetup();
+    $now = new \DateTime();
+    $currentMonth = date('F');
+
+    if($nextMeetup) {
+      $nextMeetupDateTime = \Meetup::getNextMeetup()->getStartDateTime();
+    } else {
+      $defaultStartDay    = \Config::get('edpug.day');
+      $defaultStartTime   = \Config::get('edpug.time');
+      $defaultRepetition  = \Config::get('edpug.day_repetition');
+
+      $defaultStartDateTime = new \DateTime($defaultRepetition. ' '. $defaultStartDay .' of '. $currentMonth);
+      $defaultStartDateTime = new \DateTime($defaultStartDateTime->format('Y-m-d'). ' '. $defaultStartTime);
+
+      if($now > $defaultStartDateTime){
+        $month = date('F', strtotime('+1 months'));
+      } else {
+        $month = date('F');
+      }
+
+      $nextMeetupDateTime = new \DateTime($defaultRepetition. ' '. $defaultStartDay .' of '. $month);
+    }
+
+    return $nextMeetupDateTime;
   }
 
 }
