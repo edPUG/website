1897eba4f495c972b99624c9d6c171cb4a73d4df
Stephen Heron <stephen.heron@gmail.com>
2013-08-19 19:05:47 +0100

    Added ASCII art to HTML

---
+++ app/views/includes/ascii.blade.php
@@ -0,0 +1,24 @@
+<!--           
+          _ _____  _    _  _____ 
+         | |  __ \| |  | |/ ____|
+  ___  __| | |__) | |  | | |  __ 
+ / _ \/ _` |  ___/| |  | | | |_ |
+|  __/ (_| | |    | |__| | |__| |
+ \___|\__,_|_|     \____/ \_____|
+
+----------------------------------------
+
+Thanks to:
+Ally Burns: about.me/allyburns
+Jamie Robertson
+Phil Brooks
+Stephen Heron: www.stephenheron.co.uk
+Vicky Sinclair: www.vickysinclair.com
+
+----------------------------------------
+
+UP UP DOWN DOWN LEFT RIGHT LEFT RIGHT A B 
+
+----------------------------------------
+
+--!>
--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+
+@include('includes.ascii')
+
     <meta charset="utf-8">
     <title>
     @section('title')
