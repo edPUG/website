acbc04afc7302b4583561b3a809d4db18752aa9a
Alistair Burns <alistair.burns@line.uk.com>
2014-08-19 14:21:13 +0100

    updated entry mechanism to use a tweet id

--- app/controllers/CompetitionController.php
+++ app/controllers/CompetitionController.php
@@ -2,48 +2,46 @@
 
 class CompetitionController extends BaseController {
 
-	public function getEntriesJson()
-	{
-		$handle   = 'edpug';
-		$testMode = (bool) Input::get('test', false);
-		$hashtag = Input::get('hashtag', strtolower(date('My')) . 'comp');
+    public function getEntriesJson()
+    {
 
-		$searchQuery = $testMode ? "@{$handle}" : "@{$handle} #{$hashtag}";
+        $ids = array_filter(explode(',', Input::get('ids', '501713374896734209')));
 
-		$results = Twitter::getSearch(array('q' => $searchQuery, 'format' => 'array'));
+        // one entry per user (speaker gets an extra entry via edpug twitter, or via possible query param)
 
-		// one entry per user (speaker gets an extra entry via edpug twitter, or via possible query param)
+        $entrants = array();
 
-		$entrants = array();
+        foreach ($ids as $id) {
 
-		foreach ($results['statuses'] as $tweet) {
+            $id = trim($id);
 
-			// is this a retweet? don't consider it
-			if ($tweet['retweeted']) {
-				continue;
-			}
+            if (!is_numeric($id)) {
+                continue;
+            }
 
-			// user must be following edpug!
-			if (!$testMode && !$tweet['user']['following']) {
-				continue;
-      }
+            $results = Twitter::getRts($id, array('format' => 'array'));
 
-      //Switch out the normal image for a larger version
-      $tweet['user']['profile_image_url'] = str_replace('_normal.png', '.png', $tweet['user']['profile_image_url']);
+            foreach ($results as $tweet) {
 
-			// grab only certain values from the returned data
-			// and group by screen name to remove duplicates
+                //Switch out the normal image for a larger version
+                $tweet['user']['profile_image_url'] = str_replace('_normal.png', '.png', $tweet['user']['profile_image_url']);
 
-			$entrants[$tweet['user']['screen_name']] = array_intersect_key(
-				$tweet['user'],
-				array_flip(array('name', 'screen_name', 'profile_image_url'))
-			) + array_intersect_key(
-				$tweet,
-				array_flip(array('text', 'source', 'created_at'))
-			);
+                // grab only certain values from the returned data
+                // and group by screen name to remove duplicates
 
-		}
-		return Response::json(array_values($entrants));
+                $entrants[$tweet['user']['screen_name']] = array_intersect_key(
+                        $tweet['user'],
+                        array_flip(array('name', 'screen_name', 'profile_image_url'))
+                    ) + array_intersect_key(
+                        $tweet,
+                        array_flip(array('text', 'source', 'created_at'))
+                    );
+
+            }
+
+        }
+
+        return Response::json(array_values($entrants));
     }
 
 }
