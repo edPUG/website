661511a54fd0646fb196189fe87fbcd1b7c00401
Stephen Heron <stephen.heron@gmail.com>
2015-12-14 21:16:40 +0000

    Update DateHelper.php

--- app/lib/edPUG/Helpers/DateHelper.php
+++ app/lib/edPUG/Helpers/DateHelper.php
@@ -7,7 +7,7 @@ class DateHelper {
   public static function timeUntilNextMeetup()
   {
     $difference = self::calculateDifference();
-    if($difference->format('%a') < 1){
+    if($difference->format('%a') <= 1){
       $timeRemaining = $difference->format('%h hours, %i minutes');
     } else {
       $difference = self::calculateDifference(true);
